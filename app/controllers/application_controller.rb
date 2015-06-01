51f238a129eab99fc2e5f82841e6f576e2386de5
Pål Andre Sundt <pal@rubynor.com>
2015-06-01 13:22:09 +0200

    Use layout angular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def angular
-    render text: '', layout: 'application'
+    render text: '', layout: 'angular'
   end
 
 end
