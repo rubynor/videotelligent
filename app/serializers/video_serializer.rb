a6b28bc6671ee7fed6056bae96555ec1d08dfd3e
Pål Andre Sundt <pal@rubynor.com>
2015-06-04 17:09:44 +0200

    Add video_path to video

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,7 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :link, :title, :published_at, :likes, :dislikes, :uid, :thumbnail_url, :views
+  attributes :id, :link, :title, :published_at, :likes, :dislikes, :uid, :thumbnail_url, :views, :download_path
+
+  def download_path
+    download_video_path(object)
+  end
 end
