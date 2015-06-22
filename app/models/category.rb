abe63c1a4bc5a88fadeaebdd60bd6f5d14ce520d
Hilde Vestol <hilde@rubynor.com>
2015-06-22 16:55:33 +0200

    Use compact for nil category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ActiveRecord::Base
   def self.import_all
-    Video.uniq.pluck(:category_title).map do |c|
-      next unless c
+    Video.uniq.pluck(:category_title).compact.map do |c|
       Category.find_or_create_by!(name: c)
     end
   end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Category, type: :model do
       Category.import_all
     end.to change { Category.count }.by(1)
   end
+
+  it 'ignores create category for nil category_title' do
+    create :video, category_title: nil
+    expect do
+      Category.import_all
+    end.to_not change { Category.count }
+  end
 end
