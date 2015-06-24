2f33e941355175c8beca5238d9d7e1ad146ba0bb
Karianne Berg <karianne.berg@gmail.com>
2015-06-24 15:40:34 +0200

    passord uten æøå

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require Rails.root.join("config/smtp")
 
 Videotelligent::Application.configure do
   config.middleware.insert_after(::Rack::Runtime, "::Rack::Auth::Basic", "Production") do |u, p|
-    [u, p] == ['video', 'fire ponnier gjør ingen kavalkade']
+    [u, p] == ['video', 'fire ponnier er ingen kavalkade']
   end
 end
 
