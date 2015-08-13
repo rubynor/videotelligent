590614eb36a75e4d78329366962de292dc4421ec
Karianne Berg <karianne.berg@gmail.com>
2015-08-13 16:13:14 +0200

    Refactored importing into separate importers

--- app/models/youtube_repository.rb
+++ app/models/youtube_repository.rb
@@ -3,8 +3,8 @@ require 'viddl-rb'
 
 class YoutubeRepository
   def self.import_all
-    import_categories
-    import_videos
+    Youtube::CategoryImporter.new.import_all
+    Youtube::VideoImporter.new.import_all
   end
 
   def self.download_link(video)
@@ -13,60 +13,4 @@ class YoutubeRepository
     "#{yt[:url]}&title=#{filename}"
   end
 
-  private
-  def self.import_videos
-    ContentProvider.all.map do |cp|
-      account = Yt::Account.new(access_token: cp.token, refresh_token: cp.refresh_token)
-      account.content_owners.map do |content_owner|
-        content_owner.partnered_channels.map do |channel|
-
-          begin
-            channel.videos.map do |yt_video|
-              video = to_video_from(Yt::Video.new(id: yt_video.id, auth: content_owner))
-              video.save
-              video
-            end
-          rescue Yt::Errors::RequestError => e
-            if e.kind['code'] == 404
-              []
-            else
-              raise e
-            end
-          end
-        end
-      end
-    end.flatten
-  end
-
-  def self.import_categories
-    yt_categories = Yt::Collections::VideoCategories.new.where(part: 'snippet', region_code: 'US')
-
-    yt_categories.each do |yt_category|
-
-      # TODO: Replace with a select when this PR is merged: https://github.com/Fullscreen/yt/pull/119
-      if yt_category.snippet.data['assignable']
-        category = Category.find_or_initialize_by(external_reference: yt_category.id)
-        category.name = yt_category.title
-        category.save
-      end
-    end
-  end
-
-
-  def self.to_video_from(yt_video)
-    video = Video.find_or_initialize_by(uid: yt_video.id)
-
-    video.uid = yt_video.id
-    video.title = yt_video.title
-    video.views = yt_video.view_count
-    video.views_last_week = yt_video.views(since: 1.week.ago)[:total]
-    video.likes = yt_video.like_count
-    video.dislikes = yt_video.dislike_count
-    video.thumbnail_url = yt_video.thumbnail_url(:medium)
-    video.description = yt_video.description
-    video.published_at = yt_video.published_at
-    video.channel_title = yt_video.channel_title
-    video.category = Category.find_by_external_reference(yt_video.category_id)
-    video
-  end
 end
