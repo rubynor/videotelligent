c685bce4874a26bf9f9df06f5b054fffba2b054a
Karianne Berg <karianne.berg@gmail.com>
2015-09-24 13:24:33 +0200

    bugfixes

--- app/importers/youtube/video_importer.rb
+++ app/importers/youtube/video_importer.rb
@@ -41,9 +41,10 @@ module Youtube
 
     def import_video(yt_video)
       YoutubeRequestCounter.reset!
-      video = Video.find_or_initialize_by(uid: yt_video.id)
 
-      return unless yt_video.views[:total] >= 1000
+      return unless yt_video.views[:total] && yt_video.views[:total] >= 1000
+
+      video = Video.find_or_initialize_by(uid: yt_video.id)
 
       puts "Starting import of video #{yt_video.title}"
       starting_time = Time.now.to_f
@@ -66,130 +67,16 @@ module Youtube
 
       # Import "baseline": all views
 
-      baseline_date = 8.days.ago
 
       all_view_stats = []
 
+
       ViewStat.transaction do
-        puts "Importing baseline"
-
-        baseline_views_in_countries_we_care_about = Hash[[:male, :female].map do |gender|
-                                                           [gender, Hash[%w(65- 35-44 45-54 13-17 25-34 55-64 18-24).map { |age_group| [age_group, 0] }]]
-                                                         end]
-
-        yt_video.views(by: :country, until: baseline_date).select { |country, _| COUNTRIES_WE_CARE_ABOUT.include?(country) }.each do |country, total_views|
-          yt_video.viewer_percentage(in: { country: country }, until: baseline_date).each do |gender, percentages|
-            percentages.each do |age_group, percentage|
-              view_stat = ViewStat.new(video_id: video.id,
-                                       country: country,
-                                       gender: gender,
-                                       age_group: age_group,
-                                       on_date: LONG_TIME_AGO)
-
-              number_of_views = ((total_views * percentage) / 100).round
-              view_stat.number_of_views = number_of_views
-
-
-              puts "gender: #{gender.class}"
-              puts baseline_views_in_countries_we_care_about.keys.map(&:class)
-              puts "baseline_views: #{baseline_views_in_countries_we_care_about}"
-              puts "is baseline views nil? #{baseline_views_in_countries_we_care_about.nil?}"
-              puts "is baseline gender nil? #{baseline_views_in_countries_we_care_about[gender].nil?}"
-              puts "is age group nil? #{baseline_views_in_countries_we_care_about[gender][age_group].nil?}"
-
-              baseline_views_in_countries_we_care_about[gender][age_group] += number_of_views
-              all_view_stats << view_stat
-            end
-          end
-        end
-
-        puts "# of requests after baseline Europe: #{YoutubeRequestCounter.number_of_requests}"
-
-        # Insert combined "leftover rows" for baseline countries that are not part of COUNTRIES_WE_CARE_ABOUT
-        puts "Importing leftover rows"
-        views_in_all_countries = Hash[yt_video.viewer_percentage(until: baseline_date).map do |gender, dist|
-                                        [gender, Hash[dist.map do |age_group, percentage|
-                                                        [age_group, ((percentage * yt_video.views(until: baseline_date)[:total]) / 100).round]
-                                                      end]]
-                                      end]
-        [:male, :female].product(%w(65- 35-44 45-54 13-17 25-34 55-64 18-24)).each do |gender, age_group|
-          view_stat = ViewStat.new(video_id: video.id,
-                                   country: 'OTHER',
-                                   gender: gender,
-                                   age_group: age_group,
-                                   on_date: LONG_TIME_AGO)
-          view_stat.number_of_views = (views_in_all_countries[gender][age_group] || 0) - baseline_views_in_countries_we_care_about[gender][age_group]
-          all_view_stats << view_stat
-        end
-
-        puts "# of requests after baseline notEurope: #{YoutubeRequestCounter.number_of_requests}"
+        import_baseline_for_video(all_view_stats, video, yt_video)
 
         # Import separately per day for the last week
         (1.week.ago.to_date...Date.today).each do |day|
-          puts "\t Importing for #{day}"
-
-          day_views_in_countries_we_care_about = Hash[[:male, :female].map do |gender|
-                                                             [gender, Hash[%w(65- 35-44 45-54 13-17 25-34 55-64 18-24).map { |age_group| [age_group, 0] }]]
-                                                           end]
-
-          yt_video.views(by: :country, since: day, until: day).select { |country, _| COUNTRIES_WE_CARE_ABOUT.include?(country) }.each do |country, total_views|
-            puts "\t\t Importing country #{country}"
-
-            # TODO: Only pull daily viewer percentage if daily views is higher than threshold
-            viewer_percentage = if total_views > 150 #todo check this number out
-                                  yt_video.viewer_percentage(in: { country: country }, since: day, until: day)
-                                else
-                                  {}
-                                end
-            if viewer_percentage.empty?
-              puts "\t\t\tno stats for #{day} in #{country}, extrapolating using stats for last week"
-              # This means that Youtube didn't want to give us statistics for a single day and country, so we're going to fetch the week stats and extrapolate on day
-              viewer_percentage = yt_video.viewer_percentage(in: { country: country }, since: 1.week.ago)
-              if viewer_percentage.empty?
-                puts "\t\t\tno stats for last week in #{country} either, extrapolating using stats for all time"
-                viewer_percentage = yt_video.viewer_percentage(in: { country: country })
-              end
-            end
-
-            viewer_percentage.each do |gender, percentages|
-              percentages.each do |age_group, percentage|
-                puts "\t\t\t Importing #{gender}s aged #{age_group}: #{percentage}% of total"
-                number_of_views = ((total_views * percentage) / 100).round
-
-                if number_of_views > 0
-                  view_stat = ViewStat.new(video_id: video.id,
-                                           country: country,
-                                           gender: gender,
-                                           age_group: age_group,
-                                           on_date: day)
-
-                  view_stat.number_of_views = number_of_views
-                  day_views_in_countries_we_care_about[gender][age_group] += number_of_views
-                  all_view_stats << view_stat
-                end
-              end
-            end
-
-            # Insert combined "leftover rows" for given day for countries that are not part of COUNTRIES_WE_CARE_ABOUT
-            views_in_all_countries = Hash[yt_video.viewer_percentage(since: day, until: day).map do |gender, dist|
-                                            [gender, Hash[dist.map do |age_group, percentage|
-                                                            [age_group, ((percentage * yt_video.views(since: day, until: day)[:total]) / 100).round]
-                                                          end]]
-                                          end]
-            [:male, :female].product(%w(65- 35-44 45-54 13-17 25-34 55-64 18-24)).each do |gender, age_group|
-              next unless views_in_all_countries && views_in_all_countries[gender]
-
-              view_stat = ViewStat.new(video_id: video.id,
-                                       country: 'OTHER',
-                                       gender: gender,
-                                       age_group: age_group,
-                                       on_date: day)
-
-              view_stat.number_of_views = (views_in_all_countries[gender][age_group] || 0) - day_views_in_countries_we_care_about[gender][age_group]
-              all_view_stats << view_stat
-            end
-          end
-
+          import_video_stats_for_day(day, all_view_stats, video, yt_video)
         end
       end
 
@@ -201,6 +88,123 @@ module Youtube
       YoutubeRequestCounter.reset!
 
       video
+    end
+
+    def import_video_stats_for_day(day, all_view_stats, video, yt_video)
+      puts "\t Importing for #{day}"
+
+      day_views_in_countries_we_care_about = Hash[[:male, :female].map do |gender|
+                                                    [gender, Hash[%w(65- 35-44 45-54 13-17 25-34 55-64 18-24).map { |age_group| [age_group, 0] }]]
+                                                  end]
+
+      yt_video.views(by: :country, since: day, until: day).select { |country, _| COUNTRIES_WE_CARE_ABOUT.include?(country) }.each do |country, total_views|
+        puts "\t\t Importing country #{country}"
+
+        next if total_views == 0
+
+        # TODO: Only pull daily viewer percentage if daily views is higher than threshold
+        viewer_percentage = if total_views > 150 #todo check this number out
+                              yt_video.viewer_percentage(in: { country: country }, since: day, until: day)
+                            else
+                              {}
+                            end
+        if viewer_percentage.empty?
+          puts "\t\t\tno stats for #{day} in #{country}, extrapolating using stats for last week"
+          # This means that Youtube didn't want to give us statistics for a single day and country, so we're going to fetch the week stats and extrapolate on day
+          viewer_percentage = yt_video.viewer_percentage(in: { country: country }, since: 1.week.ago)
+          if viewer_percentage.empty?
+            puts "\t\t\tno stats for last week in #{country} either, extrapolating using stats for all time"
+            viewer_percentage = yt_video.viewer_percentage(in: { country: country })
+          end
+        end
+
+        viewer_percentage.each do |gender, percentages|
+          percentages.each do |age_group, percentage|
+            number_of_views = ((total_views * percentage) / 100).round
+
+            if number_of_views > 0
+              view_stat = ViewStat.new(video_id: video.id,
+                                       country: country,
+                                       gender: gender,
+                                       age_group: age_group,
+                                       on_date: day)
+
+              view_stat.number_of_views = number_of_views
+              day_views_in_countries_we_care_about[gender][age_group] += number_of_views
+              all_view_stats << view_stat
+            end
+          end
+        end
+      end
+
+      # Insert combined "leftover rows" for given day for countries that are not part of COUNTRIES_WE_CARE_ABOUT
+      day_views_in_all_countries = Hash[yt_video.viewer_percentage(since: day, until: day).map do |gender, dist|
+                                          [gender, Hash[dist.map do |age_group, percentage|
+                                                          [age_group, (((percentage || 0) * (yt_video.views(since: day, until: day)[:total] || 0)) / 100).round]
+                                                        end]]
+                                        end]
+      [:male, :female].product(%w(65- 35-44 45-54 13-17 25-34 55-64 18-24)).each do |gender, age_group|
+        next unless day_views_in_all_countries && day_views_in_all_countries[gender]
+
+        view_stat = ViewStat.new(video_id: video.id,
+                                 country: 'OTHER',
+                                 gender: gender,
+                                 age_group: age_group,
+                                 on_date: day)
+
+        view_stat.number_of_views = (day_views_in_all_countries[gender][age_group] || 0) - day_views_in_countries_we_care_about[gender][age_group]
+        all_view_stats << view_stat
+      end
+    end
+
+    def import_baseline_for_video(all_view_stats, video, yt_video)
+      baseline_date = 8.days.ago
+      puts "Importing baseline"
+
+      baseline_views_in_countries_we_care_about = Hash[[:male, :female].map do |gender|
+                                                         [gender, Hash[%w(65- 35-44 45-54 13-17 25-34 55-64 18-24).map { |age_group| [age_group, 0] }]]
+                                                       end]
+
+      yt_video.views(by: :country, until: baseline_date).select { |country, _| COUNTRIES_WE_CARE_ABOUT.include?(country) }.each do |country, total_views|
+        yt_video.viewer_percentage(in: { country: country }, until: baseline_date).each do |gender, percentages|
+          percentages.each do |age_group, percentage|
+            view_stat = ViewStat.new(video_id: video.id,
+                                     country: country,
+                                     gender: gender,
+                                     age_group: age_group,
+                                     on_date: LONG_TIME_AGO)
+
+            number_of_views = ((total_views * percentage) / 100).round
+            view_stat.number_of_views = number_of_views
+
+            baseline_views_in_countries_we_care_about[gender][age_group] += number_of_views
+            all_view_stats << view_stat
+          end
+        end
+      end
+
+      puts "# of requests after baseline Europe: #{YoutubeRequestCounter.number_of_requests}"
+
+      # Insert combined "leftover rows" for baseline countries that are not part of COUNTRIES_WE_CARE_ABOUT
+      puts "Importing leftover rows"
+      views_in_all_countries = Hash[yt_video.viewer_percentage(until: baseline_date).map do |gender, dist|
+                                      [gender, Hash[dist.map do |age_group, percentage|
+                                                      [age_group, ((percentage * yt_video.views(until: baseline_date)[:total]) / 100).round]
+                                                    end]]
+                                    end]
+      [:male, :female].product(%w(65- 35-44 45-54 13-17 25-34 55-64 18-24)).each do |gender, age_group|
+        view_stat = ViewStat.new(video_id: video.id,
+                                 country: 'OTHER',
+                                 gender: gender,
+                                 age_group: age_group,
+                                 on_date: LONG_TIME_AGO)
+        next unless views_in_all_countries[gender] && views_in_all_countries[gender][age_group]
+
+        view_stat.number_of_views = (views_in_all_countries[gender][age_group] || 0) - baseline_views_in_countries_we_care_about[gender][age_group]
+        all_view_stats << view_stat
+      end
+
+      puts "# of requests after baseline notEurope: #{YoutubeRequestCounter.number_of_requests}"
     end
 
   end
