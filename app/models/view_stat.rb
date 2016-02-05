f62172c43727c0813ba97018413634fec5e667b9
Hilde Vestol <hilde@rubynor.com>
2016-02-05 14:18:36 +0100

    Cleanup

--- app/models/view_stat.rb
+++ app/models/view_stat.rb
@@ -3,23 +3,17 @@ class ViewStat < ActiveRecord::Base
   belongs_to :video
 
   def self.merge_view_stats(to: 1.week.ago.beginning_of_week, from: (to - 1.week))
-    # to_day = 1.week.ago.beginning_of_week
-    # from_day = to_day - 1.week
-
     stats_in_period = ViewStat.where(on_date: from...to)
 
     stats = stats_in_period
       .select('video_id, country, gender, age_group, sum(number_of_views) as merged_views')
       .group(:video_id, :gender, :age_group, :country)
 
-
-    stats_sum = 0
     stats.each do |stat|
       # need to temporarily set on_date to nil,
       # to prevent these new records to be
       # destroyed with the old ones..
       # TODO: Perhaps add a boolean 'merged'?
-      stats_sum += stat.merged_views
       ViewStat.create(video_id: stat.video_id,
                       gender: stat.gender,
                       age_group: stat.age_group,
@@ -29,11 +23,8 @@ class ViewStat < ActiveRecord::Base
       )
     end
 
-    # puts "Stats sum is #{stats_sum}"
-    # puts "Items to be destroy is #{stats_in_period.count}"
-    # puts "to destroy sum = #{stats_in_period.sum(:number_of_views)}"
-
     stats_in_period.destroy_all
+    # Need to update the date for all with nil
     ViewStat.where(on_date: nil).update_all(on_date: from)
     stats
   end
--- spec/models/view_stat_spec.rb
+++ spec/models/view_stat_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe ViewStat, type: :model do
-  describe 'merge_view_stats' do
-    it 'finds all videos for the week before last and merge view stat' do
+RSpec.describe ViewStat, type: :model  do
+  describe 'merge_view_stats', order: :defined do
+
+    before(:all) do
       countries = %w(NO OTHER)
       genders = %w(female male)
       age_groups = ['13-16', '18-24', '25-35']
@@ -10,7 +11,7 @@ RSpec.describe ViewStat, type: :model do
 
       beginning_of_last_week = 1.week.ago.beginning_of_week.to_date
       start = beginning_of_last_week - 1.week
-      long_time_ago = Date.today - 1.month
+      @long_time_ago = Date.today - 1.month
 
       countries.each do |country|
         genders.each do |gender|
@@ -20,7 +21,7 @@ RSpec.describe ViewStat, type: :model do
                    gender: gender,
                    age_group: age,
                    video_id: video_id,
-                   on_date: long_time_ago,
+                   on_date: @long_time_ago,
                    number_of_views: 10
 
             14.times do |n|
@@ -36,21 +37,16 @@ RSpec.describe ViewStat, type: :model do
         end
       end
 
-      initial_view_sum = ViewStat.sum(:number_of_views)
+      @initial_view_sum = ViewStat.sum(:number_of_views)
 
-      initial_stat_count = ViewStat.count
-      atr_number = countries.length * genders.length * age_groups.length
-      expected_deleted_data = atr_number*6
+      @initial_stat_count = ViewStat.count
+      @atr_number = countries.length * genders.length * age_groups.length
+      @expected_deleted_data = @atr_number*6
+    end
 
-      # puts "stats from last week is #{ViewStat.where(on_date: start..beginning_of_last_week).count}"
-      #
-      # puts "ViewStat.count is #{ViewStat.count}"
-      expect(ViewStat.merge_view_stats.length).to eq(atr_number)
-
-      # puts "stats from last week is #{ViewStat.where(on_date: start..beginning_of_last_week).count}"
-      # puts "ViewStat.count is #{ViewStat.count}"
-
-      expect(ViewStat.count).to eq(initial_stat_count - (expected_deleted_data))
+    it 'finds all videos for the week before last and merge view stat' do
+      expect(ViewStat.merge_view_stats.length).to eq(@atr_number)
+      expect(ViewStat.count).to eq(@initial_stat_count - (@expected_deleted_data))
 
       expect do
         ViewStat.merge_view_stats
@@ -58,19 +54,15 @@ RSpec.describe ViewStat, type: :model do
 
       expect(ViewStat.last.number_of_views).to eq(20 * 7)
 
-      expect(ViewStat.sum(:number_of_views)).to eq(initial_view_sum)
+      expect(ViewStat.sum(:number_of_views)).to eq(@initial_view_sum)
 
-      # puts "Before: ViewStat.count is #{ViewStat.count}"
-
-      expect(ViewStat.merge_view_stats(to: long_time_ago + 1.day).length).to eq(atr_number)
-      # puts "After: ViewStat.count is #{ViewStat.count}"
+      expect(ViewStat.merge_view_stats(to: @long_time_ago + 1.day).length).to eq(@atr_number)
 
       expect(ViewStat.last.number_of_views).to eq(10)
 
-      expect(ViewStat.count).to eq(initial_stat_count - (expected_deleted_data))
+      expect(ViewStat.count).to eq(@initial_stat_count - (@expected_deleted_data))
 
-      expect(ViewStat.sum(:number_of_views)).to eq(initial_view_sum)
-
+      expect(ViewStat.sum(:number_of_views)).to eq(@initial_view_sum)
     end
   end
 end
