5c2b142001cacd3ae8ac4cf5e364989eaff01fb0
Pål Andre Sundt <pal@rubynor.com>
2016-02-16 00:21:50 +0100

    Monkeypatch missing attributes into Yt. Short term solution

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -5,3 +5,27 @@ Yt.configure do |config|
   config.client_id = ENV['GOOGLE_CLIENT_ID']
   config.client_secret = ENV['GOOGLE_CLIENT_SECRET']
 end
+
+# Add unsupported attribute :display_name for content_owners
+# TODO: Make pullrequest with these changes
+class Yt::Collections::ContentOwners
+  private
+  def attributes_for_new_item(data)
+    {
+      owner_name:     data['id'],
+      display_name:   data['displayName'],
+      authentication: @auth.authentication
+    }
+  end
+end
+
+class Yt::Models::ContentOwner
+  attr_reader :display_name
+
+  def initialize(options = {})
+    super options
+    @owner_name = options[:owner_name]
+    @display_name = options[:display_name]
+  end
+end
+###
