1bea09528a9d2fc32564285ad969673fe7b256d9
Pål Andre Sundt <pal@rubynor.com>
2016-02-16 00:20:04 +0100

    Add relations and deligate name to content_provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
-  has_one :content_provider, foreign_key: :uid
+  has_one :content_provider, foreign_key: :uid, primary_key: :uid
+  has_many :content_owners, through: :content_provider
+  belongs_to :current_content_owner, class_name: 'ContentOwner'
+
+  delegate :name, to: :content_provider
 
   def self.from_omniauth(auth)
     user = User.find_or_initialize_by(uid: auth['uid'])
